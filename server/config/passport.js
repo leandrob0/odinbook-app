b85e94091ddacdaa2da08b8e72f37b65c019a035
leandrob0 <leandro.bovino@gmail.com>
2022-03-10 19:41:35 -0300

    changed username for email in p[assport local strategy

--- server/config/passport.js
+++ server/config/passport.js
@@ -8,10 +8,10 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 passport.use(
-  new LocalStrategy((username, password, done) => {
-    User.findOne({ username: username }).exec((err, user) => {
+  new LocalStrategy((email, password, done) => {
+    User.findOne({ email: email }).exec((err, user) => {
       if (err) return done(err);
-      if (!user) return done(null, false, { message: 'Incorrect username' });
+      if (!user) return done(null, false, { message: 'Incorrect email' });
 
       // Compares the password passed in the login form, with the hashed and salted one in the db.
       bcrypt.compare(password, user.password, (err, res) => {
