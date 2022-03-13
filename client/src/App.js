e9c7567727086f7c818d8401f9e0c8b6c5a50c9a
leandrob0 <leandro.bovino@gmail.com>
2022-03-13 18:27:46 -0300

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+/* import { useSelector } from "react-redux"; */
 
 import Homepage from './components/pages/Homepage';
 
@@ -6,10 +7,20 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<Homepage/>}/>
+        <Route exact path="/" element={<Homepage />}/>
       </Routes>
     </BrowserRouter>
   );
 }
+
+/* const RequireAuth = ({ children, redirectTo }) => {
+  const user = useSelector((state) => state.user.value);
+
+  const isAuthenticated = (user) => {
+    return user.first_name !== '';
+  };
+
+  return isAuthenticated(user) ? children : <Navigate to={redirectTo} />;
+}; */
 
 export default App;
