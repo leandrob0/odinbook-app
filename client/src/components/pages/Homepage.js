37e8b47fe227aa3c6dd7f67d40943077646980ce
leandrob0 <leandro.bovino@gmail.com>
2022-03-14 14:17:27 -0300

    merge conflict fixed

--- client/src/components/pages/Homepage.js
+++ client/src/components/pages/Homepage.js
@@ -1,8 +1,6 @@
 import { useState } from 'react';
 import Login from '../Login';
 import Register from './Register';
-import Register from '../Register';
-import logoFacebook from '../../images/simbolo-de-la-aplicacion-de-facebook.png';
 
 const Homepage = () => {
   const [modalOpen, setModalOpen] = useState(false);
