3cc9631b488281d969627879d1eef103425dd1ec
leandrob0 <leandro.bovino@gmail.com>
2022-03-29 20:54:15 -0300

    like a comment func created

--- server/controllers/commentController.js
+++ server/controllers/commentController.js
@@ -30,8 +30,23 @@ exports.create_comment = async (req, res) => {
       .populate('author')
       .populate('likes')
       .populate('comments');
-    
+
     // Returns the post with the new comment added, and every field needed.
     res.status(200).json({ post: postUpdated });
   }
+};
+
+exports.like_comment = async (req, res) => {
+  const comment = await Comment.findById(req.params.id);
+
+  for (let like in comment.likes) {
+    if (like === req.user._id) {
+      return res
+        .status(400)
+        .json({ msg: 'The user already liked that comment.' });
+    }
+  }
+
+  comment.likes = comment.likes.concat(req.user._id);
+  await Comment.findByIdAndUpdate(req.params.id, comment);
 };
--- server/routes/commentRouter.js
+++ server/routes/commentRouter.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 
 const authorizeUser = require('../middleware/authorizeUser');
-//const commentController = require('../controllers/commentController');
+const commentController = require('../controllers/commentController');
 
-router.post('/new/:id', authorizeUser);
+// POST Creates a new comment for the postId passed in the params, returns the post with the comments updated.
+router.post('/new/:id', authorizeUser, commentController.create_comment);
+
+// PUT Updates the like count of a comment.
+router.put('/like/:id', authorizeUser, commentController.like_comment);
 
 module.exports = router;
