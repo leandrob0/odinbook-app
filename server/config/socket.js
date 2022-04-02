483c42cba11eb6b2806cefa74669e08349f0e1e2
leandrob0 <leandro.bovino@gmail.com>
2022-04-02 17:51:14 -0300

    socket backend done

--- server/config/socket.js
+++ server/config/socket.js
@@ -4,11 +4,25 @@ const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server, { cors: '*' });
 
+let allUsers = [];
+
+const addNewUser = (user, socketId) => {
+  !allUsers.some((x) => x.email === user.email) &&
+    allUsers.push({ user, socketId });
+};
+
+const getUser = (email) => {
+  return allUsers.find((user) => user.email === email);
+};
+
 // Socket.io
 io.on('connection', (socket) => {
-  io.to(socket.flags.id).emit();
+  socket.on('newUser', (user) => {
+    addNewUser(user, socket.id);
+  });
 
-  socket.on('disconnect', () => {
-    console.log('Someone has left');
+  socket.on('sendFriendRequest', ({ senderUser, receiverUser }) => {
+    const receiver = getUser(receiverUser.email);
+    socket.to(receiver.socketId).emit('receiveFriendRequest', { senderUser });
   });
 });
