f2a1e74de35b9b05836fc05d2653839ec02c9328
leandrob0 <leandro.bovino@gmail.com>
2022-04-09 23:59:06 -0300

    comment like visuals done

--- client/src/components/SingleComment.js
+++ client/src/components/SingleComment.js
@@ -1,4 +1,17 @@
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+
+import { checkIfLiked } from '../helpers/checkIfLiked';
+
+import { ThumbUpIcon } from '@heroicons/react/solid';
+
 const SingleComment = ({ comment }) => {
+  const loggedUser = useSelector((state) => state.user.value);
+  const [liked, setLiked] = useState(
+    checkIfLiked(comment.likes, loggedUser.id)
+  );
+  const [amountLikes, setAmountLikes] = useState(comment.likes.length);
+
   return (
     <div className="flex w-full">
       <a
@@ -11,11 +24,29 @@ const SingleComment = ({ comment }) => {
           src={'/' + comment.author.profile_pic}
         />
       </a>
-      <div className="ml-1">
-        <h4 className="font-bold text-sm">
-          {comment.author.first_name} {comment.author.last_name}
-        </h4>
-        <p className="text-sm">{comment.text}</p>
+      <div>
+        <div className="ml-1 rounded bg-gray-200 p-2 relative">
+          <h4 className="font-bold text-sm">
+            {comment.author.first_name} {comment.author.last_name}
+          </h4>
+          <p className="text-sm">{comment.text}</p>
+          <div className="absolute right-0 -bottom-6 rounded-xl p-1 bg-white shadow-md flex justify-center items-center">
+            <ThumbUpIcon className='text-blue-600 w-5 h-5'/>
+            <p className='text-sm'>{amountLikes}</p>
+          </div>
+        </div>
+        <button
+          onClick={() => {
+            setLiked(!liked);
+            setAmountLikes(liked ? amountLikes - 1 : amountLikes + 1);
+          }}
+          type="button"
+          className={`ml-1 text-sm text-gray-800 hover:scale-110 hover:cursor-pointer transition ${
+            liked ? 'text-blue-600 font-bold' : ''
+          }`}
+        >
+          Like
+        </button>
       </div>
     </div>
   );
